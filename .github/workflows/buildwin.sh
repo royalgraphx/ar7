bab31d3752c0f7c726ebba2cb14e76b19e06ede0
RGX <39929362+royalgraphx@users.noreply.github.com>
2022-12-02 16:07:58 -0600

    Update buildwin.sh

--- .github/workflows/buildwin.sh
+++ .github/workflows/buildwin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mingw=D:/a/_temp/msys64/mingw64/
-sh ../configure --disable-guest-agent-msi --disable-werror --enable-sdl --enable-virglrenderer --enable-system --target-list=x86_64-softmmu --enable-whpx --enable-gtk --enable-libusb --extra-cflags="-I$mingw/include" --extra-ldflags="-L$mingw/lib"
+sh ../configure --disable-guest-agent-msi --disable-werror --enable-sdl --enable-opengl --enable-virglrenderer --enable-system --target-list=x86_64-softmmu --enable-whpx --enable-gtk --enable-libusb --extra-cflags="-I$mingw/include" --extra-ldflags="-L$mingw/lib"
 make
 cd ..
 zip -r qemu-sideswipe-w64.zip build-msys
